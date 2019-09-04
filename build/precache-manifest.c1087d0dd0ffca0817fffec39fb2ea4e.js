df4d4eda2ecd5f59a503fe30b8f2c144ae13f4d2
Eugene Volkov <eugenewolf507@gmail.com>
2019-09-04 16:07:55 +0300

    build

---
+++ build/precache-manifest.c1087d0dd0ffca0817fffec39fb2ea4e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb8e855e0399002b9a25db354ff87050",
+    "url": "/goit-react-hw-04-pet-store/build/index.html"
+  },
+  {
+    "revision": "b424639e9193326ef31b",
+    "url": "/goit-react-hw-04-pet-store/build/static/css/main.161e560c.chunk.css"
+  },
+  {
+    "revision": "67edc04284e4b8317c16",
+    "url": "/goit-react-hw-04-pet-store/build/static/js/2.0859de5c.chunk.js"
+  },
+  {
+    "revision": "b424639e9193326ef31b",
+    "url": "/goit-react-hw-04-pet-store/build/static/js/main.6e0320b8.chunk.js"
+  },
+  {
+    "revision": "718f29d02a7b0a682b34",
+    "url": "/goit-react-hw-04-pet-store/build/static/js/runtime~main.482a83d5.js"
+  }
+]);
